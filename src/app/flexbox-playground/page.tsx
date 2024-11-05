228c4bc9129e1b3a909f6244ac631997d2ebeab6
Harinder Singh <hrinder.maan@gmail.com>
2024-11-04 19:02:38 -0500

    fix: add flexbox playground

---
+++ src/app/flexbox-playground/page.tsx
@@ -0,0 +1,9 @@
+import FlexboxPlayground from "@/components/FlexboxPlayground/FlexboxPlayground";
+
+export default function FlexboxPlaygroundPage() {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <FlexboxPlayground />
+    </div>
+  );
+}
